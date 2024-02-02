9a972ce39daa36f2a5643fb65790c38fe0011c34
thalari sai teja <saitejathalari@gmail.com>
2024-02-02 17:03:11 -0600

    initial commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,106 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import {
+  GitHubIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  XIcon,
+} from '@/components/SocialIcons'
+import portraitImage from '@/images/IMG-main.jpg'
+
+function SocialLink({ className, href, children, icon: Icon }) {
+  return (
+    <li className={clsx(className, 'flex')}>
+      <Link
+        href={href}
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+      >
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <span className="ml-4">{children}</span>
+      </Link>
+    </li>
+  )
+}
+
+function MailIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+      />
+    </svg>
+  )
+}
+
+export const metadata = {
+  title: 'About',
+  description:
+    'I’m Spencer Sharp. I live in New York City, where I design the future.',
+}
+
+export default function About() {
+  return (
+    <Container className="mt-16 sm:mt-32">
+      <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+        <div className="lg:pl-20">
+          <div className="max-w-xs px-2.5 lg:max-w-none">
+            <Image
+              src={portraitImage}
+              alt=""
+              sizes="(min-width: 1024px) 32rem, 20rem"
+              className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+            />
+          </div>
+        </div>
+        <div className="lg:order-first lg:row-span-2">
+        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+  I'm
+  <span className="text-red-500">  Sai Teja Thalari.</span>
+  <br />Graduate Student at University of Texas at San Antonio.
+</h1>
+
+          <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+          <h3 class="text-2xl font-bold">Currently</h3>
+
+            <p>
+            A passionate individual with a love for web designing and a dream of crafting innovative web applications.
+            </p>
+            <p>
+            As I delved into the world of web designing, the process of creating websites became more than just a hobby—it became a journey of constant learning and exploration. Starting from the challenges of understanding the intricacies of web design, I gradually found joy in crafting different web pages and honing my skills.
+            </p>
+            <p>
+            Driven by a childhood ambition, I aspire to become a software engineer.
+            </p>
+            <p>
+              
+            </p>
+          </div>
+        </div>
+        <div className="lg:pl-20">
+          <ul role="list">
+            <SocialLink href="https://github.com/tejathalari" icon={GitHubIcon} className="mt-4">
+              Follow on GitHub
+            </SocialLink>
+            <SocialLink href="https://www.instagram.com/_sai._.teja/" icon={InstagramIcon} className="mt-4">
+              Follow on Instagram
+            </SocialLink>
+            <SocialLink href="www.linkedin.com/in/sai-teja-thalari-bb8263236" icon={LinkedInIcon} className="mt-4">
+              Follow on LinkedIn
+            </SocialLink>
+            <SocialLink
+              href="mailto:saitejathalari@gmail.com"
+              icon={MailIcon}
+              className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+            >
+              saitejathalari@gmail.com
+            </SocialLink>
+          </ul>
+        </div>
+      </div>
+    </Container>
+  )
+}
---
+++ src/app/uses/page.jsx
@@ -0,0 +1,97 @@
+import { Card } from '@/components/Card'
+import { Section } from '@/components/Section'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+function ToolsSection({ children, ...props }) {
+  return (
+    <Section {...props}>
+      <ul role="list" className="space-y-16">
+        {children}
+      </ul>
+    </Section>
+  )
+}
+
+function Tool({ title, href, children }) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  )
+}
+
+export const metadata = {
+  title: 'Uses',
+  description: 'Things I recommend, devices I adore, and software I use on a regular basis.',
+}
+
+export default function Uses() {
+  return (
+    <SimpleLayout
+      title="Things I recommend, devices I adore, and software I use on a regular basis."
+      intro=" Here’s a list of all of my favorite stuff."
+    >
+      <div className="space-y-20">
+        <ToolsSection title="Workstation">
+          <Tool title="14” MacBook Air, M2 chip, (2022)">
+          Unleashing the potential of sleek sophistication, my go-to workhorse is the MacBook Air with the formidable M2 chip, boasting 8GB of memory and a spacious 256GB internal storage. This lightweight powerhouse is my trusted companion, seamlessly blending efficiency with elegance. Whether diving into code, designing interfaces, or exploring data, this MacBook Air is the driving force behind my digital pursuits.
+          </Tool>
+          <Tool title="Dell Monitor">
+          Dive into the visual realm with the Dell SE2717Hr, a 27" IPS LED Full HD Monitor that graces my workspace in sleek black elegance. Its expansive screen and vibrant colors breathe life into every pixel, providing a canvas for my creativity and productivity. Whether coding, designing, or immersing myself in data, this monitor is the window to a world of digital possibilities.
+          </Tool>
+          <Tool title="Apple Magic Mouse - White">
+          Navigating the digital realm with finesse, my work companion is the White Magic Mouse with a Multi-Touch surface. Its seamless design and intuitive touch controls elevate my daily interactions with the digital canvas. From swift scrolls to effortless swipes, the Magic Mouse adds a touch of magic to my workflows, making every task a spellbinding experience.
+          </Tool>
+          <Tool title=" FWD Computer Desk - 55 inches Gaming Desk">
+          Elevating my gaming and work experiences is the FWD Computer Desk, a spacious 55-inch gaming desk that serves as the epicenter of my creative endeavors. With ample space for multiple monitors, peripherals, and a dedicated gaming setup, this desk blends form and function seamlessly. It's not just a piece of furniture; it's the command center where I bring ideas to life, conquer virtual realms, and embark on digital adventures.
+          </Tool>
+        </ToolsSection>
+        <ToolsSection title="Development tools">
+          <Tool title="Visual Studio Code">
+          Meet my coding sidekick, Visual Studio Code. Simple, powerful, and tailored to my style. Coding is an art, and with VS Code, every line is a stroke on the canvas of innovation.
+          </Tool>
+          <Tool title="Jupyter Notebook & Google Colab">
+          My data science journey is powered by Jupyter Notebook and Google Colab. These dynamic tools turn complex coding into an interactive and collaborative experience. From exploratory data analysis to machine learning experiments, they are the magical scrolls where data stories come to life.
+          </Tool>
+          <Tool title="TablePlus">
+          Navigating the intricate world of databases is seamless with TablePlus. This sleek tool simplifies my database management tasks, providing an intuitive interface and powerful features. From querying to visualization, TablePlus is the key to unlocking the potential of my data.
+          </Tool>
+          <Tool title="GitHub">
+          In the realm of version control, GitHub is my trusted ally. It's not just a platform; it's the heartbeat of my code. From collaboration to version tracking, GitHub empowers me to weave a tapestry of innovation in every repository.
+          </Tool>
+        </ToolsSection>
+        {/* <ToolsSection title="Design">
+          <Tool title="Figma">
+            We started using Figma as just a design tool but now it’s become our
+            virtual whiteboard for the entire company. Never would have expected
+            the collaboration features to be the real hook.
+          </Tool>
+        </ToolsSection>
+        <ToolsSection title="Productivity">
+          <Tool title="Alfred">
+            It’s not the newest kid on the block but it’s still the fastest. The
+            Sublime Text of the application launcher world.
+          </Tool>
+          <Tool title="Reflect">
+            Using a daily notes system instead of trying to keep things
+            organized by topics has been super powerful for me. And with
+            Reflect, it’s still easy for me to keep all of that stuff
+            discoverable by topic even though all of my writing happens in the
+            daily note.
+          </Tool>
+          <Tool title="SavvyCal">
+            Great tool for scheduling meetings while protecting my calendar and
+            making sure I still have lots of time for deep work during the week.
+          </Tool>
+          <Tool title="Focus">
+            Simple tool for blocking distracting websites when I need to just do
+            the work and get some momentum going.
+          </Tool>
+        </ToolsSection> */}
+      </div>
+    </SimpleLayout>
+  )
+}
